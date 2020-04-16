f79b94419a6be6105aa43aeed74067bd8d4c71eb
ZhouYang <zhou.yang@xinxinfinance.com>
2020-04-16 10:51:46 +0800

    修改包名称

---
+++ xwas-web/src/main/java/cn/com/xinxin/sass/web/shiro/filter/JwtLogoutFilter.java
@@ -0,0 +1,14 @@
+package cn.com.xinxin.sass.web.shiro.filter;
+
+import org.apache.shiro.web.filter.authc.LogoutFilter;
+
+/**
+ * @author: zhouyang
+ * @created: 16/04/2020.
+ * @updater:
+ * @description:
+ */
+public class JwtLogoutFilter extends LogoutFilter {
+
+
+}
