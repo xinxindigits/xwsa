9d97e0e299a31ac10a9930facf5c9cec35c84336
ZhouYang <zhou.yang@xinxinfinance.com>
2020-04-17 10:35:49 +0800

    重命名api下面的文件“ ”

--- xwas-api/src/main/java/cn/com/xinxin/sass/api/result/XSassResult.java
+++ xwas-api/src/main/java/cn/com/xinxin/sass/api/result/XSassResult.java
@@ -5,7 +5,7 @@ import cn.com.xinxin.sass.api.base.ToString;
 /**
  * Created by helloyy on 19/01/2018.
  */
-public class XPortalResult extends ToString {
+public class XSassResult extends ToString {
 
 
 }
