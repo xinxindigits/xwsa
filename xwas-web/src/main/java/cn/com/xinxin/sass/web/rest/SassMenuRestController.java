f2d2c736b36c60d8e571d6dfc0fe43ceb24c6b36
ZhouYang <zhou.yang@xinxinfinance.com>
2020-04-17 17:12:21 +0800

    增加资源类型，性别等枚举“

--- xwas-web/src/main/java/cn/com/xinxin/sass/web/rest/SassMenuRestController.java
+++ xwas-web/src/main/java/cn/com/xinxin/sass/web/rest/SassMenuRestController.java
@@ -7,9 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
  * @author: zhouyang
  * @created: 17/04/2020.
  * @updater:
- * @description: 权限管理以及对应的菜单功能管理
+ * @description: 权限管理以及对应的菜单功能管理, 对于用户的菜单权限，同时也要控制用户的按钮功能权限
  */
 @RestController
 @RequestMapping(value = "/menu",produces = "application/json; charset=UTF-8")
 public class SassMenuRestController {
+
+
 }
