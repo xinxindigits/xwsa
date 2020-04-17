739db7a2fa2d8cf4976865146c9984d6d06bc359
ZhouYang <zhou.yang@xinxinfinance.com>
2020-04-17 12:29:14 +0800

    增加权限，job，资源等相关的菜单

---
+++ xwas-web/src/main/java/cn/com/xinxin/sass/web/rest/SassMenuRestController.java
@@ -0,0 +1,15 @@
+package cn.com.xinxin.sass.web.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: zhouyang
+ * @created: 17/04/2020.
+ * @updater:
+ * @description: 权限管理以及对应的菜单功能管理
+ */
+@RestController
+@RequestMapping(value = "/menu",produces = "application/json; charset=UTF-8")
+public class SassMenuRestController {
+}
