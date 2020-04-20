3bf7906580216d4f7e64822abcc64265843f7503
ZhouYang <zhou.yang@xinxinfinance.com>
2020-04-20 16:14:52 +0800

    增加菜单权限获取接口

--- xwas-web/src/main/java/cn/com/xinxin/sass/web/rest/SassMenuRestController.java
+++ xwas-web/src/main/java/cn/com/xinxin/sass/web/rest/SassMenuRestController.java
@@ -1,7 +1,26 @@
 package cn.com.xinxin.sass.web.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import cn.com.xinxin.sass.api.enums.SassResultCodeEnum;
+import cn.com.xinxin.sass.auth.model.SassUserInfo;
+import cn.com.xinxin.sass.auth.web.AclController;
+import cn.com.xinxin.sass.biz.service.UserService;
+import cn.com.xinxin.sass.common.enums.SassBizResultCodeEnum;
+import cn.com.xinxin.sass.repository.model.ResourceDO;
+import cn.com.xinxin.sass.web.form.OrganizationForm;
+import cn.com.xinxin.sass.web.form.ResourceForm;
+import cn.com.xinxin.sass.web.utils.TreeResultUtil;
+import cn.com.xinxin.sass.web.vo.MenuTreeVO;
+import com.google.common.collect.Lists;
+import com.xinxinfinance.commons.exception.BusinessException;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * @author: zhouyang
@@ -11,7 +30,69 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping(value = "/menu",produces = "application/json; charset=UTF-8")
-public class SassMenuRestController {
+public class SassMenuRestController extends AclController {
+
+
+    private static final Logger log = LoggerFactory.getLogger(SassMenuRestController.class);
+
+
+    @Autowired
+    private UserService userService;
+
+
+    @RequestMapping(value = "/create",method = RequestMethod.POST)
+    @ResponseBody
+    @RequiresPermissions("/menu/create")
+    public Object createMenu(HttpServletRequest request,
+                                   @RequestBody ResourceForm resourceForm){
+
+
+        return null;
+
+    }
+
+    /**
+     * 用户可以看到的菜单列表
+     * @param request
+     * @return
+     */
+    @RequestMapping(value = "/routes",method = RequestMethod.GET)
+    @ResponseBody
+    public Object listAllUserMenus(HttpServletRequest request){
+
+        SassUserInfo sassUserInfo = this.getSassUser(request);
+
+        String userAccount = sassUserInfo.getAccount();
+
+        // 获取到用户的菜单资源权限等
+        List<ResourceDO> resourceDOList = this.userService.findMenusByAccount(userAccount);
+
+        if(CollectionUtils.isEmpty(resourceDOList)){
+            throw new BusinessException(SassBizResultCodeEnum.DATA_NOT_EXIST,"权限不对，无菜单数据清设置权限"
+                    ,"权限不对，无菜单数据清设置权限");
+        }
+
+        // 组装必要的参数
+        List<MenuTreeVO> menuResourceVOList = Lists.newArrayList();
+        resourceDOList.stream().forEach(
+                resourceDO -> {
+                    MenuTreeVO menuTreeVO = new MenuTreeVO();
+                    menuTreeVO.setText(resourceDO.getName());
+                    menuTreeVO.setParentId(String.valueOf(resourceDO.getParentId()));
+                    menuTreeVO.setId(String.valueOf(resourceDO.getId()));
+                    menuTreeVO.setCode(resourceDO.getCode());
+                    menuTreeVO.setUrl(resourceDO.getUrl());
+                    menuTreeVO.setHref(resourceDO.getUrl());
+                    menuTreeVO.setOrder(0);
+                    menuResourceVOList.add(menuTreeVO);
+                }
+        );
+
+
+        List<MenuTreeVO> menus = TreeResultUtil.build(menuResourceVOList);
+        // 返回数据
+        return menus;
+    }
 
 
 }
