99930fa8de0e07ed5190c7aa455b8eac05542f20
Hangzhou Liu <liu.hangzhou@xinxindigits.com>
2020-04-21 18:46:08 +0800

    develop-lhz:增加通讯录相关BO，DO转换类

---
+++ xwas-common/src/main/java/cn/com/xinxin/sass/common/constants/CommonConstants.java
@@ -0,0 +1,11 @@
+package cn.com.xinxin.sass.common.constants;
+
+/**
+ * @author: liuhangzhou
+ * @created: 2020/4/21.
+ * @updater:
+ * @description: 公用常量类
+ */
+public class CommonConstants {
+    public static final String GMT_CREATOR_SYSTEM = "system";
+}
