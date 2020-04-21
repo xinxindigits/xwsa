2dfefccd82ebb95768a2eacbef2b76ced6faa771
Hangzhou Liu <liu.hangzhou@xinxindigits.com>
2020-04-21 18:46:08 +0800

    develop-lhz:增加通讯录相关BO，DO转换类

---
+++ xwas-biz/src/main/java/cn/com/xinxin/sass/biz/convert/MemberConvert.java
@@ -0,0 +1,71 @@
+package cn.com.xinxin.sass.biz.convert;
+
+import cn.com.xinxin.sass.common.constants.CommonConstants;
+import cn.com.xinxin.sass.repository.model.MemberReceivedDO;
+import cn.com.xinxin.sass.sal.model.wechatwork.WeChatWorkUserBO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: liuhangzhou
+ * @created: 2020/4/21.
+ * @updater:
+ * @description: 成员转换类
+ */
+public class MemberConvert {
+
+    /**
+     * 将WeChatWorkUserBO转化为MemberReceivedDO
+     * @param weChatWorkUserBO  企业微信部门成员BO
+     * @param taskId 任务id
+     * @param orgId 机构id
+     * @return MemberReceivedDO
+     */
+    public static MemberReceivedDO convert2MemberReceivedDO(WeChatWorkUserBO weChatWorkUserBO,
+                                                            String taskId, String orgId) {
+        MemberReceivedDO memberReceivedDO = new MemberReceivedDO();
+        memberReceivedDO.setTaskId(taskId);
+        memberReceivedDO.setOrgId(orgId);
+        memberReceivedDO.setUserId(weChatWorkUserBO.getUserId());
+        memberReceivedDO.setMemberName(weChatWorkUserBO.getUserName());
+        memberReceivedDO.setMobile(weChatWorkUserBO.getMobile());
+        memberReceivedDO.setDepartmentIdList(weChatWorkUserBO.getDepartmentIds().toString());
+        memberReceivedDO.setOrderList(weChatWorkUserBO.getDepartmentOrders().toString());
+        memberReceivedDO.setMemberPosition(weChatWorkUserBO.getPosition());
+        memberReceivedDO.setGender(weChatWorkUserBO.getGender());
+        memberReceivedDO.setMail(weChatWorkUserBO.getEmail());
+        memberReceivedDO.setIsLeaderInDept(weChatWorkUserBO.getLeadingDepartments().toString());
+        memberReceivedDO.setAvatar(weChatWorkUserBO.getAvatar());
+        memberReceivedDO.setThumbAvatar(weChatWorkUserBO.getThumbAvatar());
+        memberReceivedDO.setTelephone(weChatWorkUserBO.getTelephone());
+        memberReceivedDO.setAlias(weChatWorkUserBO.getAlias());
+        memberReceivedDO.setStatus(weChatWorkUserBO.getStatus());
+        memberReceivedDO.setExtAttr(weChatWorkUserBO.getExternalAttributes());
+        memberReceivedDO.setQrCode(weChatWorkUserBO.getQRCode());
+        memberReceivedDO.setExternalProfile(weChatWorkUserBO.getExternalProfile());
+        memberReceivedDO.setExternalPosition(weChatWorkUserBO.getExternalPosition());
+        memberReceivedDO.setAddress(weChatWorkUserBO.getAddress());
+        memberReceivedDO.setHideMobile(weChatWorkUserBO.getHideMobile());
+        memberReceivedDO.setEnglishName(weChatWorkUserBO.getEnglishName());
+        memberReceivedDO.setOpenUserid(weChatWorkUserBO.getOpenUserId());
+        memberReceivedDO.setMainDepartment(weChatWorkUserBO.getMainDepartment());
+        memberReceivedDO.setGmtCreator(CommonConstants.GMT_CREATOR_SYSTEM);
+        return memberReceivedDO;
+    }
+
+    /**
+     * 将WeChatWorkUserBOList转化为MemberReceivedDOList
+     * @param weChatWorkUserBOS  企业微信部门成员BOList
+     * @param taskId 任务id
+     * @param orgId 机构id
+     * @return memberReceivedDOList
+     */
+    public static List<MemberReceivedDO> convert2MemberReceivedDOList(List<WeChatWorkUserBO> weChatWorkUserBOS,
+                                                                      String taskId, String orgId) {
+        List<MemberReceivedDO> memberReceivedDOList = new ArrayList<>();
+        weChatWorkUserBOS.forEach(u -> memberReceivedDOList.add(convert2MemberReceivedDO(u, taskId, orgId)));
+        return memberReceivedDOList;
+    }
+
+}
