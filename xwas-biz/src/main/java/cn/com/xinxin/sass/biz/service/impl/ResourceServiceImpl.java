b0639149545a4561c1a9c784fa3cc1d4c07cf6ce
ZhouYang <zhou.yang@xinxinfinance.com>
2020-04-24 17:33:24 +0800

    新增资源查询的权限

--- xwas-biz/src/main/java/cn/com/xinxin/sass/biz/service/impl/ResourceServiceImpl.java
+++ xwas-biz/src/main/java/cn/com/xinxin/sass/biz/service/impl/ResourceServiceImpl.java
@@ -7,6 +7,7 @@ import cn.com.xinxin.sass.repository.model.ResourceDO;
 import com.github.pagehelper.PageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 
@@ -96,5 +97,31 @@ public class ResourceServiceImpl implements ResourceService {
     @Override
     public ResourceDO findByResourceCode(String code) {
         return resourceMapper.findByResourceCode(code);
+    }
+
+
+    @Override
+    public List<ResourceDO> queryResourceTrees(String code,
+                                               String type,
+                                               String parentId) {
+
+
+        ResourceDO condition = new ResourceDO();
+
+        if(!StringUtils.isEmpty(code)){
+            condition.setCode(code);
+        }
+
+        if(!StringUtils.isEmpty(type)){
+            condition.setResourceType(type);
+        }
+
+        if(!StringUtils.isEmpty(parentId)){
+            condition.setParentId(Long.valueOf(parentId));
+        }
+
+        List<ResourceDO> resourceDOS = resourceMapper.findByCondition(condition);
+
+        return resourceDOS;
     }
 }
