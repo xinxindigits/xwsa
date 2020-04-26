6ab5a9eeb7f94c3426f6805da2d5076c2f4b16cf
范华 <fan.hua@xinxinfinance.com>
2020-04-26 20:28:39 +0800

    资源权限

---
+++ sass-admin/src/views/system/resource/function.vue
@@ -0,0 +1,147 @@
+<template>
+  <Card>
+    <Row :gutter="20" class="row-operation">
+      <Col
+        ><Button
+          icon="md-add"
+          type="primary"
+          @click="hdlCreate"
+          :disabled="!curTreeData.id"
+          >新增</Button
+        >
+      </Col>
+    </Row>
+    <Table
+      stripe
+      border
+      ref="tables"
+      :data="currentData"
+      :columns="columns"
+      @on-selection-change="hdlSelectionChange"
+    >
+      <template slot-scope="{ row }" slot="action">
+        <Button
+          type="primary"
+          size="small"
+          style="margin-right: 5px"
+          @click="hdlEdit(row)"
+        >
+          修改
+        </Button>
+        <Button
+          type="error"
+          size="small"
+          @click="hdlDelete({ parentId: row.parentId, list: [row.id] })"
+        >
+          删除
+        </Button>
+      </template>
+    </Table>
+    <resource-func-modify
+      ref="modifyModal"
+      v-model="showModal"
+      :type="modifyType"
+      @on-create-resource="hdlModified"
+      @on-update-resource="hdlModified"
+    ></resource-func-modify
+  ></Card>
+</template>
+
+<script>
+import { deleteResource } from "@/api/data";
+import ResourceFuncModify from "./modify";
+export default {
+  name: "resource-list-functions",
+  components: {
+    ResourceFuncModify
+  },
+  props: {
+    tableData: {
+      type: Array
+    },
+    curTreeData: {
+      type: Object,
+      default: function() {
+        return {
+          id: ""
+        };
+      }
+    }
+  },
+  data() {
+    return {
+      modifyType: "create",
+      showModal: false,
+      currentData: [],
+      columns: [
+        { title: "权限值", key: "authority", align: "center" },
+        { title: "名称", key: "name", align: "center" },
+        {
+          title: "操作",
+          slot: "action",
+          width: 150,
+          align: "center"
+        }
+      ]
+    };
+  },
+  methods: {
+    hdlSelectionChange() {},
+    hdlEdit(data) {
+      this.modifyType = "update";
+      this.$refs.modifyModal.setData(data);
+      this.showModal = true;
+    },
+    hdlCreate() {
+      let self = this;
+      this.modifyType = "create";
+      this.$refs.modifyModal.setData({
+        parentId: self.curTreeData.id,
+        authority: "",
+        code: "",
+        extension: "",
+        name: "",
+        resourceType: "",
+        url: ""
+      });
+      this.showModal = true;
+    },
+    hdlDelete(data) {
+      console.log(data);
+      let self = this;
+      if (data.list.length > 0) {
+        this.$Modal.confirm({
+          title: "确认删除？",
+          content: `确定删除选中记录?`,
+          onOk() {
+            deleteResource({ id: data.list[0] }).then(() => {
+              this.$Message.success("删除成功！");
+              self.$emit("on-delete-resource", data);
+            });
+          }
+        });
+      } else {
+        this.$Message.warning("请选择一条记录!");
+      }
+    },
+    hdlModified(type) {
+      this.$emit("resource-modified", type);
+    }
+  },
+  watch: {
+    tableData: {
+      deep: true,
+      immediate: true,
+      handler(newValue) {
+        this.currentData = newValue;
+      }
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.row-operation {
+  padding: 10px 0;
+}
+</style>
--- sass-admin/src/views/system/resource/resource.vue
+++ sass-admin/src/views/system/resource/resource.vue
@@ -1,11 +1,173 @@
 <template>
-  <div></div>
+  <div>
+    <Row :gutter="10">
+      <Col span="6">
+        <Card>
+          <Tree
+            ref="tree"
+            :data="treedata"
+            @on-select-change="hdlTreeSelected"
+          ></Tree>
+        </Card>
+      </Col>
+      <Col span="8">
+        <Card>
+          <Form
+            ref="form1"
+            :model="form1"
+            label-colon
+            :label-width="100"
+            :rules="rules"
+          >
+            <FormItem label="上级ID" prop="parentId">
+              <Input v-model="form1.parentId" disabled></Input>
+            </FormItem>
+            <FormItem label="名称" prop="name">
+              <Input v-model="form1.name"></Input>
+            </FormItem>
+            <FormItem label="资源权限值" prop="authority">
+              <Input v-model="form1.authority"></Input>
+            </FormItem>
+            <FormItem label="组件URI" prop="url">
+              <Input v-model="form1.url"></Input>
+            </FormItem>
+            <FormItem label="描述" prop="extension">
+              <Input v-model="form1.extension"></Input>
+            </FormItem>
+            <FormItem>
+              <Button type="primary" @click="hdlqModifyMenu">修改</Button>
+            </FormItem>
+          </Form>
+        </Card>
+      </Col>
+      <Col span="8">
+        <resource-list-function
+          :table-data="tableData"
+          @on-delete-resource="query"
+          :cur-tree-data="selectedTreeData"
+          @resource-modified="hdlResourceModified"
+        >
+        </resource-list-function>
+      </Col>
+    </Row>
+  </div>
 </template>
 
 <script>
+import ResourceListFunction from "./function";
+import { getResourceMenuTree, getResourceQueryTree } from "@/api/data";
 export default {
-  name: "resource-list"
+  name: "resource-list",
+  components: {
+    ResourceListFunction
+  },
+  data() {
+    return {
+      treedata: [],
+      selectedTreeData: {},
+      tableData: [],
+      form1: {
+        parentId: "",
+        name: "",
+        code: "",
+        url: "",
+        authority: "",
+        extension: ""
+      },
+      rules: {
+        name: [{ required: true, message: "名称不能为空", trigger: "blur" }],
+        url: [{ required: true, message: "URI不能为空", trigger: "blur" }],
+        authority: [
+          { required: true, message: "权限值不能为空", trigger: "blur" }
+        ]
+      },
+      styles: {
+        height: "calc(100% - 55px)",
+        overflow: "auto",
+        paddingBottom: "53px",
+        position: "static"
+      }
+    };
+  },
+  methods: {
+    query(data) {
+      this.tableData = [];
+      if (!data) {
+        let arr = this.$refs.tree.getSelectedNodes();
+        if (arr.length == 0) {
+          return;
+        }
+        data = this.$refs.tree.getSelectedNodes()[0];
+      }
+      getResourceQueryTree({ parentId: data.id }, { silent: true })
+        .then(res => {
+          this.tableData = res.data;
+          this.selectedTreeData = data;
+        })
+        .catch(() => {
+          this.tableData = [];
+          this.selectedTreeData = data;
+        });
+    },
+    hdlTreeSelected(i, n) {
+      console.log(n);
+      n.extension = n.extension || "";
+      this.form1 = this._.pick(
+        n,
+        "code",
+        "parentId",
+        "authority",
+        "name",
+        "url",
+        "extension"
+      );
+      this.query(n);
+    },
+    hdlqModifyMenu() {},
+    formatData(data) {
+      let arr = [];
+      let self = this;
+      data.forEach(item => {
+        let { text: title, expanded: expand, children: funcs } = item;
+        let obj = {
+          title,
+          name: title,
+          expand,
+          funcs,
+          ...self._.pick(
+            item,
+            "code",
+            "parentId",
+            "authority",
+            "url",
+            "id",
+            "resourceType"
+          )
+        };
+        if (item.children && item.children.length > 0) {
+          obj.children = this.formatData(item.children);
+        }
+        arr.push(obj);
+      });
+      return arr;
+    },
+    hdlResourceModified(type) {
+      this.$Message.success(
+        `${type == "create" ? "新增成功！" : "修改成功！"}`
+      );
+      this.query();
+    }
+  },
+  mounted() {
+    getResourceMenuTree({}).then(res => {
+      this.treedata = this.formatData(res.data);
+    });
+  }
 };
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.row-operation {
+  padding: 10px 0;
+}
+</style>
