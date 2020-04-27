cb0f17f76c9a6f7f949a6b0c5d75c77b86776c6e
ZhouYang <zhou.yang@xinxinfinance.com>
2020-04-27 16:56:40 +0800

    完成微信部门的查询接口

--- xwas-web/src/main/java/cn/com/xinxin/sass/web/form/WechatOrgQueryForm.java
+++ xwas-web/src/main/java/cn/com/xinxin/sass/web/form/WechatOrgQueryForm.java
@@ -12,39 +12,11 @@ public class WechatOrgQueryForm extends ToString {
 
     private static final long serialVersionUID = -3924428072428332894L;
 
-    private long id;
-
-    private String orgId;
-
     private String departmentId;
 
     private String departmentName;
 
     private String englishName;
-
-    private String parentId;
-
-    private int departmentOrder;
-
-    private String status;
-
-    private String extension;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getOrgId() {
-        return orgId;
-    }
-
-    public void setOrgId(String orgId) {
-        this.orgId = orgId;
-    }
 
     public String getDepartmentId() {
         return departmentId;
@@ -70,35 +42,5 @@ public class WechatOrgQueryForm extends ToString {
         this.englishName = englishName;
     }
 
-    public String getParentId() {
-        return parentId;
-    }
 
-    public void setParentId(String parentId) {
-        this.parentId = parentId;
-    }
-
-    public int getDepartmentOrder() {
-        return departmentOrder;
-    }
-
-    public void setDepartmentOrder(int departmentOrder) {
-        this.departmentOrder = departmentOrder;
-    }
-
-    public String getExtension() {
-        return extension;
-    }
-
-    public void setExtension(String extension) {
-        this.extension = extension;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
 }
