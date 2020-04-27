c626b4f0b315e34f8908859141dd3dd4f14f5f8f
ZhouYang <zhou.yang@xinxinfinance.com>
2020-04-27 12:44:05 +0800

    增加微信部门查询接口

---
+++ xwas-web/src/main/java/cn/com/xinxin/sass/web/form/WechatQueryForm.java
@@ -0,0 +1,16 @@
+package cn.com.xinxin.sass.web.form;
+
+import cn.com.xinxin.sass.api.base.ToString;
+
+/**
+ * @author: zhouyang
+ * @created: 27/04/2020.
+ * @updater:
+ * @description:
+ */
+public class WechatQueryForm extends ToString {
+
+    private static final long serialVersionUID = -3924428072428332894L;
+
+
+}
