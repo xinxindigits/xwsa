83ab0d961494e00689b247c0695e2cde50c3f99f
Hangzhou Liu <liu.hangzhou@xinxindigits.com>
2020-04-29 17:29:09 +0800

    develop-lhz：增加查询会话记录api

--- xwas-biz/src/main/java/cn/com/xinxin/sass/biz/service/MsgRecordService.java
+++ xwas-biz/src/main/java/cn/com/xinxin/sass/biz/service/MsgRecordService.java
@@ -1,5 +1,6 @@
 package cn.com.xinxin.sass.biz.service;
 
+import cn.com.xinxin.sass.common.model.PageResultVO;
 import cn.com.xinxin.sass.repository.model.MsgRecordDO;
 
 import java.util.List;
@@ -18,4 +19,16 @@ public interface MsgRecordService {
      * @return 插入记录条数
      */
     int insertBatch(List<MsgRecordDO> msgRecordDOS);
+
+    /**
+     * 通过机构id，userid，消息发送时间范围查询记录
+     * @param userId 用户id
+     * @param startTime 消息发送时间范围之起始时间
+     * @param endTime 消息发送时间范围之终止时间
+     * @param page 分页信息
+     * @param orgId 机构id
+     * @return 消息记录
+     */
+    PageResultVO<MsgRecordDO> queryByOrgIdAndMemberUserIdAndTime(String userId, String startTime,
+                                                                  String endTime, PageResultVO page, String orgId);
 }
