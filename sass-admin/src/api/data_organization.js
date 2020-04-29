93238b0479e7d6db186dd8bb87f8c1e6588dff57
yanghaoxian <yang.haoxian@xinxindigits.com>
2020-04-29 10:10:09 +0800

    lint自动修复

--- sass-admin/src/api/data_organization.js
+++ sass-admin/src/api/data_organization.js
@@ -1,42 +1,89 @@
 import axios from "@/libs/api.request";
 
 export const getOrganizationList = data => {
-    return axios.request({
-        url: "organization/list",
-        data,
-        method: "post"
-    });
+  return axios.request({
+    url: "organization/list",
+    data,
+    method: "post"
+  });
 };
 
-export const queryOrganization = ({code}) => {
-    return axios.request({
-        url: "organization/query/" + code,
-        method: "get"
-    });
+export const queryOrganization = ({ code }) => {
+  return axios.request({
+    url: "organization/query/" + code,
+    method: "get"
+  });
 };
-
 
 export const delOrganization = ({ codes }) => {
-    let data = { codes };
-    return axios.request({
-        url: "organization/delete",
-        data,
-        method: "post",
-    });
+  let data = { codes };
+  return axios.request({
+    url: "organization/delete",
+    data,
+    method: "post"
+  });
 };
-export const addOrganization = ({ code, name, corpId,state,privateKey,addressListSecret,customerContactSecret,chatRecordSecret,remark,parentId,orgType }) => {
-    let data = { code, name, corpId,state,privateKey,addressListSecret,customerContactSecret,chatRecordSecret,remark,parentId,orgType };
-    return axios.request({
-        url: "organization/create",
-        data,
-        method: "post"
-    });
+export const addOrganization = ({
+  code,
+  name,
+  corpId,
+  state,
+  privateKey,
+  addressListSecret,
+  customerContactSecret,
+  chatRecordSecret,
+  remark,
+  parentId,
+  orgType
+}) => {
+  let data = {
+    code,
+    name,
+    corpId,
+    state,
+    privateKey,
+    addressListSecret,
+    customerContactSecret,
+    chatRecordSecret,
+    remark,
+    parentId,
+    orgType
+  };
+  return axios.request({
+    url: "organization/create",
+    data,
+    method: "post"
+  });
 };
-export const updateOrganization = ({  code, name, corpId,state,privateKey,addressListSecret,customerContactSecret,chatRecordSecret,remark,parentId,orgType }) => {
-    let data = {  code, name, corpId,state,privateKey,addressListSecret,customerContactSecret,chatRecordSecret,remark,parentId,orgType  };
-    return axios.request({
-        url: "organization/update",
-        data,
-        method: "post"
-    });
+export const updateOrganization = ({
+  code,
+  name,
+  corpId,
+  state,
+  privateKey,
+  addressListSecret,
+  customerContactSecret,
+  chatRecordSecret,
+  remark,
+  parentId,
+  orgType
+}) => {
+  let data = {
+    code,
+    name,
+    corpId,
+    state,
+    privateKey,
+    addressListSecret,
+    customerContactSecret,
+    chatRecordSecret,
+    remark,
+    parentId,
+    orgType
+  };
+  return axios.request({
+    url: "organization/update",
+    data,
+    method: "post"
+  });
 };
