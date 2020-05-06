6fd0f95a3c255180aff7abc06a508d4301c82659
ZhouYang <zhou.yang@xinxinfinance.com>
2020-05-06 12:55:38 +0800

    增加标签创建功能

--- xwas-biz/src/main/java/cn/com/xinxin/sass/biz/service/impl/TagsServiceImpl.java
+++ xwas-biz/src/main/java/cn/com/xinxin/sass/biz/service/impl/TagsServiceImpl.java
@@ -1,5 +1,10 @@
 package cn.com.xinxin.sass.biz.service.impl;
 
+import cn.com.xinxin.sass.biz.service.TagsService;
+import cn.com.xinxin.sass.repository.dao.TagsDOMapper;
+import cn.com.xinxin.sass.repository.dao.TagsRelationsDOMapper;
+import cn.com.xinxin.sass.repository.model.TagsDO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -12,5 +17,18 @@ import org.springframework.stereotype.Service;
 public class TagsServiceImpl implements TagsService {
 
 
+    @Autowired
+    private TagsDOMapper tagsDOMapper;
 
+    @Autowired
+    private TagsRelationsDOMapper tagsRelationsDOMapper;
+
+
+
+    @Override
+    public int createTags(TagsDO tagsDO) {
+
+        return tagsDOMapper.insertSelective(tagsDO);
+        
+    }
 }
