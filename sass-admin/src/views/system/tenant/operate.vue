c600d14cd6cc0a5a46ab72648bbcdc0d83ed36a3
yanghaoxian <yang.haoxian@xinxindigits.com>
2020-05-12 16:05:55 +0800

    查询租户任务配置

---
+++ sass-admin/src/views/system/tenant/operate.vue
@@ -0,0 +1,19 @@
+<template>
+    <Modal
+        v-model="curValue"
+        :title="title"
+        :loading="true"
+        :mask-closable="false"
+    >
+    </Modal>
+</template>
+
+<script>
+    export default {
+        name: "operate"
+    }
+</script>
+
+<style scoped>
+
+</style>
