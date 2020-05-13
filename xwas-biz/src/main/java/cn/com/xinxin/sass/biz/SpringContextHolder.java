5091955b70a0a144862551545965c6342f13ced2
Hangzhou Liu <liu.hangzhou@xinxindigits.com>
2020-05-13 12:00:54 +0800

    develop-lhz:修改sdk的引入

--- xwas-biz/src/main/java/cn/com/xinxin/sass/biz/SpringContextHolder.java
+++ xwas-biz/src/main/java/cn/com/xinxin/sass/biz/SpringContextHolder.java
@@ -76,8 +76,4 @@ public class SpringContextHolder implements ApplicationContextAware, DisposableB
     private static void assertContextInjected() {
         Validate.validState(applicationContext != null, "applicaitonContext属性未注入, 请在applicationContext.xml中定义SpringContextHolder.");
     }
-
-    static {
-        System.loadLibrary("WeWorkFinanceSdk_Java");
-    }
 }
