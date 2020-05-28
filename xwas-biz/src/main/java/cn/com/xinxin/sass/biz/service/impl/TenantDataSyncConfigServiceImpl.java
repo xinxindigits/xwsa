e7e0e68b85eae5d90134a287791a922886c01fd4
ZhouYang <zhou.yang@xinxinfinance.com>
2020-05-28 19:00:28 +0800

    解决合并代码冲突

--- xwas-biz/src/main/java/cn/com/xinxin/sass/biz/service/impl/TenantDataSyncConfigServiceImpl.java
+++ xwas-biz/src/main/java/cn/com/xinxin/sass/biz/service/impl/TenantDataSyncConfigServiceImpl.java
@@ -1,9 +1,9 @@
 package cn.com.xinxin.sass.biz.service.impl;
 
-import cn.com.xinxin.sass.biz.service.OrgDataSyncConfigService;
+import cn.com.xinxin.sass.biz.service.TenantDataSyncConfigService;
 import cn.com.xinxin.sass.common.enums.SassBizResultCodeEnum;
-import cn.com.xinxin.sass.repository.dao.OrgDataSyncConfigDOMapper;
-import cn.com.xinxin.sass.repository.model.OrgDataSyncConfigDO;
+import cn.com.xinxin.sass.repository.dao.TenantDataSyncConfigDOMapper;
+import cn.com.xinxin.sass.repository.model.TenantDataSyncConfigDO;
 import com.xinxinfinance.commons.exception.BusinessException;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -20,14 +20,14 @@ import org.springframework.transaction.annotation.Transactional;
  * @description: 机构同步配置信息DB服务
  */
 @Service
-public class OrgDataSyncConfigServiceImpl implements OrgDataSyncConfigService {
+public class TenantDataSyncConfigServiceImpl implements TenantDataSyncConfigService {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(OrgDataSyncConfigServiceImpl.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(TenantDataSyncConfigServiceImpl.class);
 
-    private final OrgDataSyncConfigDOMapper orgDataSyncConfigDOMapper;
+    private final TenantDataSyncConfigDOMapper tenantDataSyncConfigDOMapper;
 
-    public OrgDataSyncConfigServiceImpl(final OrgDataSyncConfigDOMapper orgDataSyncConfigDOMapper) {
-        this.orgDataSyncConfigDOMapper = orgDataSyncConfigDOMapper;
+    public TenantDataSyncConfigServiceImpl(final TenantDataSyncConfigDOMapper tenantDataSyncConfigDOMapper) {
+        this.tenantDataSyncConfigDOMapper = tenantDataSyncConfigDOMapper;
     }
 
     /**
@@ -37,7 +37,7 @@ public class OrgDataSyncConfigServiceImpl implements OrgDataSyncConfigService {
      * @return 机构同步配置信息
      */
     @Override
-    public OrgDataSyncConfigDO selectByOrgIdAndTaskType(String orgId, String taskType) {
+    public TenantDataSyncConfigDO selectByOrgIdAndTaskType(String orgId, String taskType) {
         if (StringUtils.isBlank(orgId)) {
             LOGGER.error("通过机构id和任务类型查询记录, orgId不能为空");
             throw new BusinessException(SassBizResultCodeEnum.ILLEGAL_PARAMETER,
@@ -48,34 +48,34 @@ public class OrgDataSyncConfigServiceImpl implements OrgDataSyncConfigService {
             throw new BusinessException(SassBizResultCodeEnum.ILLEGAL_PARAMETER,
                     "通过机构id和任务类型查询记录, taskType不能为空");
         }
-        OrgDataSyncConfigDO orgDataSyncConfigDO = orgDataSyncConfigDOMapper.selectByOrgIdAndTaskType(orgId, taskType);
+        TenantDataSyncConfigDO tenantDataSyncConfigDO = tenantDataSyncConfigDOMapper.selectByOrgIdAndTaskType(orgId, taskType);
 
-        if (null == orgDataSyncConfigDO) {
+        if (null == tenantDataSyncConfigDO) {
             LOGGER.error("无法通过机构id[{}]找到机构同步[{}]任务配置信息", orgId, taskType);
             throw new BusinessException(SassBizResultCodeEnum.DATA_NOT_EXIST, "找不到机构同步任务配置信息");
         }
 
-        return orgDataSyncConfigDO;
+        return tenantDataSyncConfigDO;
     }
 
     /**
      * 根据id更新记录
-     * @param orgDataSyncConfigDO 记录
+     * @param tenantDataSyncConfigDO 记录
      * @return 更新成功的条数
      */
     @Override
     @Transactional(propagation = Propagation.REQUIRES_NEW, rollbackFor = Exception.class)
-    public int updateById(OrgDataSyncConfigDO orgDataSyncConfigDO) {
-        if (null == orgDataSyncConfigDO) {
+    public int updateById(TenantDataSyncConfigDO tenantDataSyncConfigDO) {
+        if (null == tenantDataSyncConfigDO) {
             LOGGER.error("根据id更新记录记录, orgDataSyncConfigDO不能为空");
             throw new BusinessException(SassBizResultCodeEnum.ILLEGAL_PARAMETER,
                     "根据id更新记录记录, orgDataSyncConfigDO不能为空");
         }
-        if (null == orgDataSyncConfigDO.getId()) {
+        if (null == tenantDataSyncConfigDO.getId()) {
             LOGGER.error("根据id更新记录记录, id不能为空");
             throw new BusinessException(SassBizResultCodeEnum.ILLEGAL_PARAMETER,
                     "根据id更新记录记录, id不能为空");
         }
-        return orgDataSyncConfigDOMapper.updateByPrimaryKeySelective(orgDataSyncConfigDO);
+        return tenantDataSyncConfigDOMapper.updateByPrimaryKeySelective(tenantDataSyncConfigDO);
     }
 }
---
+++ xwas-web/src/main/java/cn/com/xinxin/sass/web/vo/TenantInfoVO.java
@@ -0,0 +1,14 @@
+package cn.com.xinxin.sass.web.vo;
+
+import cn.com.xinxin.sass.api.base.ToString;
+
+/**
+ * @author: zhouyang
+ * @created: 30/04/2020.
+ * @updater:
+ * @description:
+ */
+public class TenantInfoVO extends ToString{
+
+
+}
