e45dde02cc36c129f0efae36962592a25713388a
ZhouYang <zhou.yang@xinxinfinance.com>
2020-05-28 19:01:07 +0800

    解决合并代码冲突

--- xwas-web/src/main/java/cn/com/xinxin/sass/web/rest/SassUserRestController.java
+++ xwas-web/src/main/java/cn/com/xinxin/sass/web/rest/SassUserRestController.java
@@ -174,6 +174,9 @@ public class SassUserRestController extends AclController {
 
         userCreateDO.setGender(Byte.valueOf(String.valueOf(userForm.getGender())));
 
+        // FIXME: 先默认设置为xinxin租户
+        userCreateDO.setTenantId("xinxin");
+
         int result = this.userService.createUser(userCreateDO);
 
 
