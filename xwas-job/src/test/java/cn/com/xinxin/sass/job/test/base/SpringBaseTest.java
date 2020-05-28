f236ae69e82251191538afe49e3ba7551c22d37a
ZhouYang <zhou.yang@xinxinfinance.com>
2020-05-28 18:40:17 +0800

    解决合并代码冲突

---
+++ xwas-job/src/test/java/cn/com/xinxin/sass/job/test/base/SpringBaseTest.java
@@ -0,0 +1,20 @@
+package cn.com.xinxin.sass.job.test.base;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by helloyy on 08/02/2018.
+ */
+
+@ContextConfiguration(locations = {"classpath:spring-application-test.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
+public class SpringBaseTest {
+
+    @Test
+    public void testOK(){
+
+    }
+}
---
+++ xwas-job/src/test/java/cn/com/xinxin/sass/job/test/service/UserDODODORoleServiceTest.java
@@ -0,0 +1,52 @@
+package cn.com.xinxin.sass.job.test.service;
+
+import cn.com.xinxin.sass.biz.service.UserRoleService;
+import cn.com.xinxin.sass.repository.model.UserRoleDO;
+import cn.com.xinxin.sass.job.test.base.SpringBaseTest;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Arrays;
+
+/**
+ * Created by dengyunhui on 2018/5/1
+ **/
+public class UserDODODORoleServiceTest extends SpringBaseTest {
+
+    @Autowired
+    private UserRoleService userRoleService;
+
+    @Test
+    @Ignore
+    public void TestCreateUserRole(){
+        UserRoleDO userRoleDO = new UserRoleDO();
+        userRoleDO.setUserAccount("asdfasf");
+        userRoleDO.setRoleCode("fsfdsf");
+
+        UserRoleDO userRoleDO1 = userRoleService.createUserRole(userRoleDO);
+    }
+
+    @Test
+    @Ignore
+    public void testBatchInsert(){
+        UserRoleDO userRoleDO = new UserRoleDO();
+        userRoleDO.setUserAccount("asdfasf1");
+        userRoleDO.setRoleCode("fsfdsf1");
+        userRoleDO.setExtension("");
+        userRoleDO.setGmtCreator("");
+        userRoleDO.setGmtUpdater("");
+
+        UserRoleDO userRoleDO2 = new UserRoleDO();
+        userRoleDO2.setUserAccount("asdfasf2");
+        userRoleDO2.setRoleCode("fsfdsf2");
+        userRoleDO2.setExtension("");
+        userRoleDO2.setGmtCreator("");
+        userRoleDO2.setGmtUpdater("");
+
+        boolean rst = userRoleService.createUserRoles(Arrays.asList(userRoleDO,userRoleDO2));
+        Assert.assertTrue(rst);
+    }
+
+}
