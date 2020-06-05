de1e0861ba896ea3b1bdad3d4ea0d023c2842aa3
ZhouYang <zhou.yang@xinxinfinance.com>
2020-06-05 16:12:12 +0800

    删除乱码注释

--- xwas-sal/src/main/java/com/tencent/wework/Finance.java
+++ xwas-sal/src/main/java/com/tencent/wework/Finance.java
@@ -44,43 +44,25 @@ public class Finance {
 	
     public native static void DestroySdk(long sdk);
     public native static long NewSlice();
-    /**
-     * @brief �ͷ�slice����NewSlice�ɶ�ʹ��
-     * @return 
-     */
+
     public native static void FreeSlice(long slice);
 
-    /**
-     * @brief ��ȡslice����
-     * @return ����
-     */
+
     public native static String GetContentFromSlice(long slice);
 
-    /**
-     * @brief ��ȡslice���ݳ���
-     * @return ����
-     */
+
     public native static int GetSliceLen(long slice);
     public native static long NewMediaData();
     public native static void FreeMediaData(long mediaData);
 
-    /**
-     * @brief ��ȡmediadata outindex
-     * @return outindex
-     */
+
     public native static String GetOutIndexBuf(long mediaData);
-    /**
-     * @brief ��ȡmediadata data����
-     * @return data
-     */
+
     public native static byte[] GetData(long mediaData);
     public native static int GetIndexLen(long mediaData);
     public native static int GetDataLen(long mediaData);
 
-    /**
-     * @brief �ж�mediadata�Ƿ����
-     * @return 1��ɡ�0δ���
-     */
+  
     public native static int IsMediaDataFinish(long mediaData);
 
     static {
